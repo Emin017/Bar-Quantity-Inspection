fb9e05bde8e6e81c60e13d1d7393266ab02314cc
Chu Qiming <1346550347@qq.com>
2023-12-12 09:05:11 +0800

    features: Add select image and image process

---
+++ Demo2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Demo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.open_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(1, 2);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(619, 525);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(619, 525);
+            // 
+            // open_button
+            // 
+            this.open_button.Location = new System.Drawing.Point(703, 550);
+            this.open_button.Name = "open_button";
+            this.open_button.Size = new System.Drawing.Size(162, 60);
+            this.open_button.TabIndex = 1;
+            this.open_button.Text = "选择图片";
+            this.open_button.UseVisualStyleBackColor = true;
+            this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 778);
+            this.Controls.Add(this.open_button);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button open_button;
+    }
+}
+
---
+++ Demo2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace Demo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HObject ho_image, ho_GrayImage, ho_ImageScaleMax, ho_Region1;
+        HObject ho_RegionErosion;
+        private void open_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HTuple hv_width = new HTuple(), hv_Height = new HTuple();
+                HTuple hv_UsedThreshold = new HTuple();
+
+                string image_Path;
+                OpenFileDialog opnDlg = new OpenFileDialog();
+                opnDlg.Filter = "All image files |*.bmp;*.jpg;*.gif;*.png";
+                if (opnDlg.ShowDialog() == DialogResult.OK)
+                {
+                    image_Path = opnDlg.FileName;
+                    HOperatorSet.ReadImage(out ho_image, image_Path);
+                    HOperatorSet.Rgb1ToGray(ho_image, out ho_GrayImage);
+                    HOperatorSet.ScaleImageMax(ho_GrayImage, out ho_ImageScaleMax);
+
+                    HOperatorSet.BinaryThreshold(ho_ImageScaleMax, out ho_Region1, "max_separability", 
+          "light", out hv_UsedThreshold);
+                    HOperatorSet.ErosionCircle(ho_Region1, out ho_RegionErosion, 20);
+                    HObject ExpTmpOutVar_0;
+                    HOperatorSet.ErosionCircle(ho_RegionErosion, out ExpTmpOutVar_0, 20);
+                    ho_Region1 = ExpTmpOutVar_0;
+
+                    HOperatorSet.GetImageSize(ho_image, out hv_width, out hv_Height);
+                    HOperatorSet.SetPart(hWindowControl1.HalconWindow, 0, 0, hv_Height - 1, hv_width - 1);
+                    HOperatorSet.DispObj(ho_Region1, hWindowControl1.HalconWindow);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Program Error!");
+            }
+        }
+    }
+}
