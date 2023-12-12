f23ed1f5b898a7b4ab9c7673bbfdbf82ab1ef829
Chu Qiming <1346550347@qq.com>
2023-12-12 09:09:21 +0800

    feature: Add ROI and count button

--- Demo2/Form1.Designer.cs
+++ Demo2/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.hWindowControl1 = new HalconDotNet.HWindowControl();
             this.open_button = new System.Windows.Forms.Button();
+            this.ROI_Button = new System.Windows.Forms.Button();
+            this.count_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hWindowControl1
@@ -45,19 +47,39 @@
             // 
             // open_button
             // 
-            this.open_button.Location = new System.Drawing.Point(703, 550);
+            this.open_button.Location = new System.Drawing.Point(702, 74);
             this.open_button.Name = "open_button";
-            this.open_button.Size = new System.Drawing.Size(162, 60);
+            this.open_button.Size = new System.Drawing.Size(162, 44);
             this.open_button.TabIndex = 1;
             this.open_button.Text = "选择图片";
             this.open_button.UseVisualStyleBackColor = true;
             this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // ROI_Button
+            // 
+            this.ROI_Button.Location = new System.Drawing.Point(702, 149);
+            this.ROI_Button.Name = "ROI_Button";
+            this.ROI_Button.Size = new System.Drawing.Size(162, 44);
+            this.ROI_Button.TabIndex = 2;
+            this.ROI_Button.Text = "ROI选择";
+            this.ROI_Button.UseVisualStyleBackColor = true;
+            // 
+            // count_button
+            // 
+            this.count_button.Location = new System.Drawing.Point(702, 230);
+            this.count_button.Name = "count_button";
+            this.count_button.Size = new System.Drawing.Size(162, 44);
+            this.count_button.TabIndex = 3;
+            this.count_button.Text = "统计计数";
+            this.count_button.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(932, 778);
+            this.Controls.Add(this.count_button);
+            this.Controls.Add(this.ROI_Button);
             this.Controls.Add(this.open_button);
             this.Controls.Add(this.hWindowControl1);
             this.Name = "Form1";
@@ -70,6 +92,8 @@
 
         private HalconDotNet.HWindowControl hWindowControl1;
         private System.Windows.Forms.Button open_button;
+        private System.Windows.Forms.Button ROI_Button;
+        private System.Windows.Forms.Button count_button;
     }
 }
 
